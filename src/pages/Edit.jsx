792b7dca2d50486d6283f37bed01f92ff42e7638
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-12-21 15:11:23 -0800

    Added Text component and added draggable and edit functionality

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Text from '../components/Text';
+import { Button } from 'react-bootstrap';
 import { useSearchParams } from 'react-router-dom';
 const EditPage = () => {
   const [params] = useSearchParams();
+  const [count, setCount] = useState(0);
   console.log(params.get('url'));
+
+  const addText = () => {
+    setCount(count + 1);
+  };
   return (
     <div>
-      <img src={params.get('url')} width='250 px' />
+      <div>
+        <img src={params.get('url')} width='250 px' />
+        {Array(count)
+          .fill(0)
+          .map((e) => (
+            <Text />
+          ))}
+      </div>
+      <Button onClick={addText}>Add Text</Button>
     </div>
   );
 };
